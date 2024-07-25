04986ad41e74a861501dc9db61ade6de3f93cf1e
polcaballero <pol.caballero@urbidermis.com>
2024-07-25 13:29:46 +0200

    ESP32_GENERIC_S3_SPIRAM

---
+++ ports/esp32/boards/ESP32_GENERIC_S3_SPIRAM/mpconfigboard.cmake
@@ -0,0 +1,11 @@
+set(IDF_TARGET esp32s3)
+
+set(SDKCONFIG_DEFAULTS
+    ${SDKCONFIG_DEFAULTS}
+    boards/sdkconfig.240mhz
+    boards/sdkconfig.spiram_oct
+)
+
+list(APPEND MICROPY_DEF_BOARD
+    MICROPY_HW_BOARD_NAME="Generic ESP32S3 module with Octal-SPIRAM"
+)
